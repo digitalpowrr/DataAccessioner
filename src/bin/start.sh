a3de7a61eb9ae9a5325de1bef78e13c3297bbc08
sprater <sprater@gmailcom>
2024-02-10 17:47:05 -0600

    Set FITS_HOME

--- src/bin/start.sh
+++ src/bin/start.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-cd "$(dirname "$0")"
-java -Dlog4j.configurationFile=da-log-config.xml -Xmx1024m -jar dataaccessioner.jar
+cd "$(dirname "$0")" || exit
+
+if [ "X${FITS_HOME}" == "X" ]
+then
+    FITS_HOME="$(dirname "$0")"
+    export FITS_HOME
+fi
+
+FITS_HOME="${FITS_HOME}" java -Dlog4j.configurationFile=da-log-config.xml -Xmx1024m -jar dataaccessioner.jar
